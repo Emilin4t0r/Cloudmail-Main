105fa177c622446c97c6780d12867c8b2f10965f
Emilin4t0r <virolainen.emil@gmail.com>
2021-11-25 12:45:47 +0200

    Possible fix for smooth camera jitter

--- Assets/Scripts/Camera/Cam3D.cs
+++ Assets/Scripts/Camera/Cam3D.cs
@@ -7,29 +7,15 @@ public class Cam3D : MonoBehaviour {
 
 	public Transform target;
 	public float smoothTime = 0.3f;
-	private float origSmoothTime;
 	private Vector3 velocity = Vector3.zero;
-	
-	void Start() {
-		origSmoothTime = smoothTime;
-	}
 
-
-	void Update() {
+    void LateUpdate() {
 		transform.rotation = Quaternion.LookRotation(target.position - transform.position, Vector3.up);
 
 		// Define a target position above and behind the target transform
 		Vector3 targetPosition = target.TransformPoint(new Vector3(0, 2f, -10f));
 
 		// Smoothly move the camera towards that target position
-		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
-
-		/*
-		if (controls.Back("hold")) {
-			smoothTime = 0.1f;
-		}
-		if (controls.Back("up")) {
-			smoothTime = origSmoothTime;
-		}*/
+		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);		
 	}
 }
